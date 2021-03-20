c809547bae68886dfedd085ee056191a7d1f432e
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-03-20 15:18:54 -0400

    widget, widgetservice

---
+++ src/main/java/com/example/javaservercoursemanager/models/Widget.java
@@ -0,0 +1,143 @@
+package com.example.javaservercoursemanager.models;
+
+public class Widget {
+
+  private String name;
+  private Long id;
+  private String type;
+  private String topicId;
+  private Integer widgetOrder;
+  private String text;
+  private String url;
+  private Integer size;
+  private Integer width;
+  private Integer height;
+  private String cssClass;
+  private String style;
+  private String value;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getTopicId() {
+    return topicId;
+  }
+
+  public Integer getWidgetOrder() {
+    return widgetOrder;
+  }
+
+  public void setWidgetOrder(Integer widgetOrder) {
+    this.widgetOrder = widgetOrder;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public Integer getSize() {
+    return size;
+  }
+
+  public void setSize(Integer size) {
+    this.size = size;
+  }
+
+  public Integer getWidth() {
+    return width;
+  }
+
+  public void setWidth(Integer width) {
+    this.width = width;
+  }
+
+  public Integer getHeight() {
+    return height;
+  }
+
+  public void setHeight(Integer height) {
+    this.height = height;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getStyle() {
+    return style;
+  }
+
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public Widget(String name, Long id, String type, Integer widgetOrder, String text,
+    String url, Integer size, Integer width, Integer height, String cssClass, String style,
+    String value) {
+    this.name = name;
+    this.id = id;
+    this.type = type;
+    this.widgetOrder = widgetOrder;
+    this.text = text;
+    this.url = url;
+    this.size = size;
+    this.width = width;
+    this.height = height;
+    this.cssClass = cssClass;
+    this.style = style;
+    this.value = value;
+  }
+
+  public Widget(Long id, String topicId, String type, Integer size, String text) {
+    this.id = id;
+    this.topicId = topicId;
+    this.type = type;
+    this.size = size;
+    this.text = text;
+  }
+}
---
+++ src/main/java/com/example/javaservercoursemanager/services/WidgetService.java
@@ -0,0 +1,66 @@
+package com.example.javaservercoursemanager.services;
+
+import com.example.javaservercoursemanager.models.Widget;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WidgetService {
+
+  private List<Widget> widgets = new ArrayList<>();
+
+  public Widget createWidget(String topicId, Widget widget) {
+    Long id = (new Date()).getTime();
+    widget.setId(id);
+    this.widgets.add(widget);
+    return widget;
+  }
+
+  public List<Widget> findAllWidgets() {
+    return widgets;
+  }
+
+  public List<Widget> findWidgetsForTopic(String topicId) {
+    return widgets.stream().filter(widget -> widget.getTopicId().equals(topicId))
+      .collect(Collectors.toList());
+  }
+
+  public Widget findWidgetById(Long id) {
+    for(Widget w: widgets) {
+      if(w.getId().equals(id)) {
+        return w;
+      }
+    }
+    return null;
+  }
+
+  public int updateWidget(Long id, Widget updatedWidget) {
+    for (int i = 0; i < this.widgets.size(); i++) {
+      Widget widget = widgets.get(i);
+      if (widget.getId() == id) {
+        this.widgets.set(i, updatedWidget);
+        return 1;
+      }
+    }
+    return -1;
+  }
+
+  public int deleteWidget(Long id) {
+    int index = -1;
+    for (int i = 0; i < widgets.size(); i++) {
+      Widget w = widgets.get(i);
+      if (w.getId().equals(id)) {
+        index = i;
+      }
+    }
+    if (index >= 0) {
+      widgets.remove(index);
+      return 1;
+    }
+    return -1;
+  }
+
+}
