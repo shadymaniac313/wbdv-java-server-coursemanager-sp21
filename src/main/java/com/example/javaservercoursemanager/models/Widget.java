1d518eb12829bca03bab1db4de65a585f41beab6
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-03-25 11:06:01 -0400

    setup db

--- src/main/java/com/example/javaservercoursemanager/models/Widget.java
+++ src/main/java/com/example/javaservercoursemanager/models/Widget.java
@@ -1,9 +1,20 @@
 package com.example.javaservercoursemanager.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "widgets")
 public class Widget {
 
-  private String name;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  private String name;
   private String type;
   private String topicId;
   private Integer widgetOrder;
